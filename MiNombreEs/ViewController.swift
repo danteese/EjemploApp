31fd9bcdefcb1fbc724ab889d6b7a9cf4dd3a012
Danteese <dalnte@me.com>
2018-10-18 17:20:56 -0500

    The label changes and set a different color

--- MiNombreEs/ViewController.swift
+++ MiNombreEs/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var number = 0
 
+    @IBOutlet weak var ScoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func increaseNumber(_ sender: Any) {
+        number = number + 1
+        ScoreLabel.text = String(number)
+        ScoreLabel.textColor = UIColor.blue
+    }
+    
 }
 
